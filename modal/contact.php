d97a491b2e7d078f941c6f90b989695de3647d5a
TimotejkoPantak <timopantakgit@gmail.com>
2023-02-13 19:38:58 +0100

    Edit contact form

--- modal/contact.php
+++ modal/contact.php
@@ -1,51 +1,31 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = strip_tags(trim($_POST["name"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["message"]);
 
-// Put contacting email here
-$php_main_email = "info@timotejpantak.com";
+    // Check for empty fields
+    if (empty($name) || empty($email) || empty($message)) {
+        http_response_code(400);
+        echo "Please fill out all fields.";
+        exit;
+    }
 
-//Fetching Values from URL
-$php_name = $_POST['ajax_name'];
-$php_email = $_POST['ajax_email'];
-$php_message = $_POST['ajax_message'];
+    // Construct the email headers
+    $to = "info@timotejpantak.com";
+    $subject = "New contact form submission";
+    $headers = "From: $name <$email>";
 
-
-
-//Sanitizing email
-$php_email = filter_var($php_email, FILTER_SANITIZE_EMAIL);
-
-
-//After sanitization Validation is performed
-if (filter_var($php_email, FILTER_VALIDATE_EMAIL)) {
-	
-	
-		$php_subject = "Message from contact form";
-		
-		$php_headers = 'From: ' . $php_email . "\r\n" .
-		'Reply-To: ' . $php_email . "\r\n" .
-		'X-Mailer: PHP/' . phpversion();
-
-		// HTML Template for sending email
-		
-		$php_template = '<div style="padding:50px;">Hello ' . $php_name . ',<br/>'
-		. 'Thank you for contacting us.<br/><br/>'
-		. '<strong style="color:#f00a77;">Name:</strong>  ' . $php_name . '<br/>'
-		. '<strong style="color:#f00a77;">Email:</strong>  ' . $php_email . '<br/>'
-		. '<strong style="color:#f00a77;">Message:</strong>  ' . $php_message . '<br/><br/>'
-		. 'This is a Contact Confirmation mail.'
-		. '<br/>'
-		. 'We will contact you as soon as possible .</div>';
-		$php_sendmessage = "<div style=\"background-color:#f5f5f5; color:#333;\">" . $php_template . "</div>";
-		
-		// message lines should not exceed 70 characters (PHP rule), so wrap it
-		$php_sendmessage = wordwrap($php_sendmessage, 70);
-		
-		// Send mail by PHP Mail Function
-		mail($php_main_email, $php_subject, $php_sendmessage, $php_headers);
-		echo "";
-	
-	
+    // Send the email
+    if (mail($to, $subject, $message, $headers)) {
+        http_response_code(200);
+        echo "Thank you! Your message has been sent.";
+    } else {
+        http_response_code(500);
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
 } else {
-	echo "<span class='contact_error'>* Invalid email *</span>";
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
 }
-
 ?>
